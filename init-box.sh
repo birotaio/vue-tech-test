baca8b11e73106d780863a45344ac34588eb3424
François Risoud <francois.risoud@gmail.com>
2021-08-30 11:27:35 +0200

    use mockapi.io instead of jsonbox, update documentation

--- init-box.sh
+++
@@ -1,9 +0,0 @@
-if [ "$#" -lt 1 ]
-then
-  echo "Usage: sh ./init-box.sh <box_id>"
-else
-  box_id=$1
-  curl -X POST "https://jsonbox.io/${box_id}" \
-    -H "content-type: application/json" \
-    -d @bikes.json
-fi
